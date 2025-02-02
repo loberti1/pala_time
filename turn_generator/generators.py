5c1c6bddfeacff0b5b740fdd14ca8c813ffb23e5
Luciano <obertiluciano@gmail.com>
2025-02-02 06:38:23 -0300

    cambios turn_generator

--- turn_generator/generators.py
+++ turn_generator/generators.py
@@ -1,57 +1,72 @@
 import os
+import time
 from random import *
-import unittest as uni
 from datetime import datetime,date
 
 class Customer:
-    def __init__(self,day,month,year,weekday):
+    def __init__(self,day,month,year,weekday,hour):
         self.day = day
         self.month = month
         self.year = year
         self.weekday = weekday
-to_day = Customer(datetime.now().day,datetime.now().month,datetime.now().year,datetime.now().weekday())
-
-def turn_generator(area):
-    PH = 1
-    PE = 1
-    CS = 1
-    while type(area) == type(str()):
-        if area == 'pharmacy':
-            yield 'PH-'+str(PH)
-            PH  = PH+1
-        elif area == 'perfumerie':
-            yield 'PE-'+str(PE)
-            PE += 1
-        elif area == 'cosmetics':
-            yield 'CS-'+str(CS)
-            CS += 1
-        else: pass
+        self.hour = hour
+    
+    def turn_generator(self,area):
+        PH = 1
+        PE = 1
+        CS = 1
+        while type(area) == type(str()):
+            if area == 'pharmacy':
+                yield 'PH-'+str(PH)
+                PH  = PH+1
+            elif area == 'perfumerie':
+                yield 'PE-'+str(PE)
+                PE += 1
+            elif area == 'cosmetics':
+                yield 'CS-'+str(CS)
+                CS += 1
+            else: pass
 
 def program_start():
-    pharmacy = turn_generator('pharmacy')
-    perfumerie = turn_generator('perfumerie')
-    cosmetics = turn_generator('cosmetics')
+    to_day = Customer(datetime.now().day,datetime.now().month,datetime.now().year,datetime.now().weekday(),datetime.now().hour)
+    pharmacy = to_day.turn_generator('pharmacy')
+    perfumerie = to_day.turn_generator('perfumerie')
+    cosmetics = to_day.turn_generator('cosmetics')
     try:
-        while (datetime.now().hour >= 9 and datetime.now().hour <= 20 or to_day.weekday != 6):
-            print(f'Today is {to_day.year}-{to_day.month}-{to_day.day}')
-            print('#####################################################')
-            print('Welcome, please take your turn in your selected area:')
+        while (to_day.hour >= 9 and to_day.hour <= 20 or to_day.weekday != 6):
+            print(f'{to_day.year}-{to_day.month}-{to_day.day}')
+            print('######################################\nWelcome, please take your turn in your selected area\n######################################')
             print('PHARMACY\nPERFUMERIE\nCOSMETICS')
             selection = str(input()).lower()
             if selection not in {'pharmacy','perfumerie','cosmetics'}:
                 os.system('cls')
-                print('Your selection is not valid, please try again') 
+                print('Your selection is not valid, please try again')
+                time.sleep(2.5) 
+                os.system('cls')
             elif selection == 'pharmacy':
                 os.system('cls')
-                print (f'Thank you for your time, your turn is: {next(pharmacy)}, you will be taken care of soon')
+                print('########################################')
+                print (f'Thank you for your time, your turn is:\n{next(pharmacy)}\nYou will be taken care of soon')
+                print('########################################')
+                time.sleep(2.5)
+                os.system('cls')
             elif selection == 'perfumerie':
                 os.system('cls')
-                print (f'Thank you for your time, your turn is: {next(perfumerie)}, you will be taken care of soon')
+                print('########################################')
+                print (f'Thank you for your time, your turn is:\n{next(perfumerie)}\nYou will be taken care of soon')
+                print('########################################')
+                time.sleep(2.5) 
+                os.system('cls')
             elif selection == 'cosmetics':
                 os.system('cls')
-                print(f'Thank you for your time, your turn is: {next(cosmetics)}, you will be taken care of soon') 
+                print('########################################')
+                print(f'Thank you for your time, your turn is:\n{next(cosmetics)}\nYou will be taken care of soon') 
+                print('########################################')
+                time.sleep(2.5)
+                os.system('cls')
             else:
                 print('a critial error has ocurred')
+                time.sleep(2.5)
     except:
         print('CRITICAL FAILURE, POWER OFF')
         raise ConnectionAbortedError
