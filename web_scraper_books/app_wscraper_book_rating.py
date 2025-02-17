fedeabe83158554a258786a4b48f28dd4cf060cd
Luciano <obertiluciano@gmail.com>
2025-02-16 21:22:36 -0300

    adding book web scraper

---
+++ web_scraper_books/app_wscraper_book_rating.py
@@ -0,0 +1,26 @@
+"""scrap books.toscrape.com to get books with rating > 4"""
+import bs4, requests as re
+url_init = 'https://books.toscrape.com/catalogue/page-{}.html'
+titles_big_rating = []
+
+#start search in first 50 pages
+for page in range(1, 51):
+
+    url_page = url_init.format(page)
+    result = re.get(url_page)
+    soup = bs4.BeautifulSoup(result.text, 'lxml')
+
+    #select book class
+    books = soup.select('.product_pod')
+
+    #iterate through books
+    for  book in books:
+
+        #get 4 or 5 stars only
+        if len(book.select('.star-rating.Four')) != 0 or len(book.select('.star-rating.Five')) != 0:
+            book_title = book.select('a')[1]['title']
+            titles_big_rating.append(book_title)
+
+#get the books
+for t in titles_big_rating:
+    print(t)
